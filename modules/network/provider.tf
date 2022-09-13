31a3a37585587169c3b08a65b923af2fe582e9a1
mikatong <mikatong@tencent.com>
2022-09-13 17:27:52 +0800

    update

--- modules/network/provider.tf
+++ modules/network/provider.tf
@@ -2,11 +2,6 @@ terraform {
     required_providers {
         tencentcloud = {
             source = "tencentcloudstack/tencentcloud"
-            version = "1.77.8"
         }
     }
-}
-
-provider "tencentcloud" {
-    region="ap-guangzhou"
 }

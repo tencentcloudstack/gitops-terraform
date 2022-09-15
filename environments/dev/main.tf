f8818c4d3b0e23ce87f1939caadd9b2e294b3bdc
mikatong <mikatong@tencent.com>
2022-09-15 17:27:36 +0800

    update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@ module "network" {
   source = "../../modules/network"
 
   vpc_name     = "simple-vpc"
-  vpc_cidr     = "10.1.0.0/16"
-  network_cidr = "10.1.0.0/24"
+  vpc_cidr     = "10.2.0.0/16"
+  network_cidr = "10.2.0.0/24"
   subnet_name  = "simple-subnet-dev"
 }

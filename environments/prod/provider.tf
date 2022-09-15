5c772ae21c6d295074fff559a07665fcefd68654
mikatong <mikatong@tencent.com>
2022-09-15 15:45:45 +0800

    prod test

---
+++ environments/prod/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_providers {
+        tencentcloud = {
+            source = "tencentcloudstack/tencentcloud"
+            version = "1.77.8"
+        }
+    }
+    backend "cos" {
+      region = "ap-guangzhou"
+      bucket = "keep-gitops-prod-1308919341"
+      prefix = "terraform/state"
+    }
+}
+
+provider "tencentcloud" {
+    region="ap-guangzhou"
+}
